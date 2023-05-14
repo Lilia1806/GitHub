601bb310c6efb6204e69eafa201a969f1a826031
Lila1806 <lilataalaybekova@gmail.com>
2023-05-14 17:01:10 +0600

    Создала детальный фрагмент и подключила ViewBinding.

--- app/src/main/java/com/example/github/ui/adapter/CinemaAdapter.kt
+++ app/src/main/java/com/example/github/ui/adapter/CinemaAdapter.kt
@@ -8,48 +8,47 @@ import com.bumptech.glide.Glide
 import com.example.github.databinding.ItemCinemaBinding
 import com.example.github.models.CinemaModel
 
-
 class CinemaAdapter(
-        val onItemClick: (model: CinemaModel) -> Unit
-    ) : RecyclerView.Adapter<CinemaAdapter.FirstViewHolder>() {
+    val onItemClick: (model: CinemaModel) -> Unit
+) : RecyclerView.Adapter<CinemaAdapter.FirstViewHolder>() {
 
-        private var list: List<CinemaModel> = ArrayList()
+    private var list: List<CinemaModel> = ArrayList()
 
-        @SuppressLint("NotifyDataSetChanged")
-        fun setList(listTwo: List<CinemaModel>) {
-            this.list = listTwo
-            notifyDataSetChanged()
+    @SuppressLint("NotifyDataSetChanged")
+    fun setList(listTwo: List<CinemaModel>) {
+        this.list = listTwo
+        notifyDataSetChanged()
 
-        }
-
-        inner class FirstViewHolder(private val binding: ItemCinemaBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-
-            init {
-                itemView.setOnClickListener {
-                    onItemClick(list[adapterPosition])
-                }
-            }
-
-            fun onBind(model: CinemaModel) {
-                binding.txt.text = model.name
-                Glide.with(binding.image.context).load(model.name).into(binding.image)
-            }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FirstViewHolder {
-            return FirstViewHolder(
-                ItemCinemaBinding.inflate(
-                    LayoutInflater.from(parent.context),
-                    parent,
-                    false
-                )
-            )
-        }
-
-        override fun onBindViewHolder(holder: FirstViewHolder, position: Int) {
-            holder.onBind(list[position])
-        }
-
-        override fun getItemCount(): Int = list.size
     }
+
+    inner class FirstViewHolder(private val binding: ItemCinemaBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener {
+                onItemClick(list[adapterPosition])
+            }
+        }
+
+        fun onBind(model: CinemaModel) {
+            binding.txt.text = model.name
+            Glide.with(binding.image.context).load(model.name).into(binding.image)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FirstViewHolder {
+        return FirstViewHolder(
+            ItemCinemaBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: FirstViewHolder, position: Int) {
+        holder.onBind(list[position])
+    }
+
+    override fun getItemCount(): Int = list.size
+}
--- app/src/main/java/com/example/github/ui/fragments/CinemaFragment.kt
+++ app/src/main/java/com/example/github/ui/fragments/CinemaFragment.kt
@@ -1,12 +1,10 @@
 package com.example.github.ui.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.github.R
 import com.example.github.databinding.FragmentCinemaBinding
 
 class CinemaFragment : Fragment() {
